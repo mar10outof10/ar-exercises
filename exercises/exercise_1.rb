3d6e689a3da0a4739f6d74182aaa48648570df04
Martin Halas <echoes.time@gmail.com>
2021-04-27 22:58:01 +0000

    Added code for first 3 exercises

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,23 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Store.create do |s|
+  s.name = "Burnaby"
+  s.annual_revenue = 300000
+  s.mens_apparel = true
+  s.womens_apparel = true
+end
+Store.create do |s|
+  s.name = "Richmond"
+  s.annual_revenue = 1260000
+  s.mens_apparel = false
+  s.womens_apparel = true
+end
+Store.create do |s|
+  s.name = "Gastown"
+  s.annual_revenue = 190000
+  s.mens_apparel = true
+  s.womens_apparel = false
+end
+
+puts Store.count
